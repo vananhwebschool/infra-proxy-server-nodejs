9aed1a5cc764c89621002b0df9cb45fca1d789b5
tvanh4 <tvanh4@myboost.co>
2024-04-26 01:31:09 +0200

    add load balancer + 2nd node app

--- another-app/server.js
+++ another-app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-const port = 8080;
+const port = 9000;
 
 router.use(function (req,res,next) {
     console.log('/' + req.method);
@@ -11,13 +11,13 @@ router.use(function (req,res,next) {
 
 router.get('/', function(req,res){
     // res.sendFile(path + 'home.html');
-    res.send({title:"home1"})
+    res.send({title:"home2"})
     res.status(200);
 });
 
 router.get('/about', function(req,res){
     // res.sendFile(path + 'about.html');
-    res.send({title:"about1"})
+    res.send({title:"about2"})
     res.status(200);
 
 });
@@ -25,5 +25,5 @@ router.get('/about', function(req,res){
 app.use('/', router);
 
 app.listen(port, function () {
-    console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 9000!')
 })
